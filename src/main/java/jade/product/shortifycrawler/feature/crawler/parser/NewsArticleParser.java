e70fa4240ab01d5aa4196cdc17be91da04b13f98
이태현 <jade@egiskorea.com>
2026-01-22 13:47:39 +0900

    fix: 기사 원문 publish 시간 LocalDateTime now => 실제 발행된 시간을 크롤링하여 반환

--- src/main/java/jade/product/shortifycrawler/feature/crawler/parser/NewsArticleParser.java
+++ src/main/java/jade/product/shortifycrawler/feature/crawler/parser/NewsArticleParser.java
@@ -3,9 +3,11 @@ package jade.product.shortifycrawler.feature.crawler.parser;
 import jade.product.shortifycrawler.feature.crawler.dto.ArticleContent;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class NewsArticleParser implements ArticleParser {
@@ -35,12 +37,22 @@ public class NewsArticleParser implements ArticleParser {
                 ? doc.selectFirst("a.media_end_head_top_logo img").attr("alt")
                 : "UNKNOWN";
 
+        LocalDateTime publishedAt = null;
+        Element publishedEl = doc.selectFirst(
+                "span.media_end_head_info_datestamp_time._ARTICLE_DATE_TIME"
+        );
+        if (publishedEl != null) {
+            publishedAt = LocalDateTime.parse(
+                    publishedEl.attr("data-date-time"),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            );
+        }
         return ArticleContent.builder()
                 .url(url)
                 .title(title)
                 .content(content)
                 .press(press)
-                .publishedAt(LocalDateTime.now())
+                .publishedAt(publishedAt)
                 .build();
     }
 }
